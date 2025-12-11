4e557023c245b70c1b5029f7f66f3e0021cedfeb
unknown <licq@g-bits.com>
2025-12-11 11:36:15 +0800

    1136

--- EO.py
+++ EO.py
@@ -2,3 +2,4 @@ import easyocr
 reader = easyocr.Reader(['en']) # this needs to run only once to load the model into memory
 result = reader.readtext("preprocessed_image.png")
 print(result)
+21312321
